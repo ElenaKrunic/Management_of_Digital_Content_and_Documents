ef09bf5a1f205f0ca9702ef49c31fc7d6c59aa02
Elena Krunic <elenakrunic@gmail.com>
2021-12-16 13:24:27 +0100

    spring initialized

---
+++ ues/src/test/java/elena/ues/UesApplicationTests.java
@@ -0,0 +1,13 @@
+package elena.ues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
